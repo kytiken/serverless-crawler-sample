d7b51c107ed0af3caecc0ea33ab7e20575d042a0
kytiken <kytiken@gmail.com>
2019-03-03 01:30:54 +0900

    implement

--- handler.ts
+++ handler.ts
@@ -1,11 +1,44 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyHandler, KinesisStreamHandler } from "aws-lambda";
+import { Kinesis } from "aws-sdk";
 
-export const hello: APIGatewayProxyHandler = async (event, context) => {
+export const hello: APIGatewayProxyHandler = async event => {
+  const kinesis = new Kinesis();
+  const kinesisStreamName = process.env.kinesisStreamName;
+  const params: Kinesis.PutRecordInput = {
+    Data:
+      "hello" /* required */ /* Strings will be Base-64 encoded on your behalf */,
+    PartitionKey: "hello-partition" /* required */,
+    StreamName: kinesisStreamName /* required */
+  };
+  await kinesis
+    .putRecord(params)
+    .promise()
+    .then(value => {
+      console.log(`ShardId: ${value.ShardId}`);
+      console.log(`EncryptionType: ${value.EncryptionType}`);
+      console.log(`SequenceNumber: ${value.SequenceNumber}`);
+    })
+    .catch(reason => {
+      console.log(reason);
+    });
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      message:
+        "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
+      input: event
+    })
   };
-}
+};
+
+export const kinesisoutput: KinesisStreamHandler = async event => {
+  event.Records.forEach(record => {
+    console.log(`record.kinesis.data: ${record.kinesis.data}`);
+    console.log(
+      `record.kinesis.data(decoded): ${new Buffer(
+        record.kinesis.data,
+        "base64"
+      ).toString()}`
+    );
+  });
+};
