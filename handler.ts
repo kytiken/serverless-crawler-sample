ad06ab900e8abdb42f490ee3bd18e8c2c5255433
kytiken <kytiken@gmail.com>
2019-03-03 23:27:30 +0900

    use sqs

--- handler.ts
+++ handler.ts
@@ -1,5 +1,5 @@
-import { APIGatewayProxyHandler, KinesisStreamHandler } from "aws-lambda";
-import { Kinesis, DynamoDB } from "aws-sdk";
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { SQS, DynamoDB } from "aws-sdk";
 import { v4 as uuid } from "uuid";
 const rp = require("request-promise");
 const dynamoDb = new DynamoDB.DocumentClient();
@@ -8,28 +8,6 @@ const getPage = (url: string) => {
   return rp.get(url).then(htmlString => {
     return htmlString;
   });
-};
-
-const putKinesisStream = (data: string) => {
-  console.log(`putKinesisStream - data: ${data}`);
-  const params: Kinesis.PutRecordInput = {
-    Data: data /* required */ /* Strings will be Base-64 encoded on your behalf */,
-    PartitionKey: process.env.SLOT_KINESIS_STREAM_PARTITION_KEY /* required */,
-    StreamName: process.env.SLOT_KINESIS_STREAM_NAME /* required */
-  };
-  console.log(params);
-  const kinesis = new Kinesis();
-  return kinesis
-    .putRecord(params)
-    .promise()
-    .then(value => {
-      console.log(`ShardId: ${value.ShardId}`);
-      console.log(`EncryptionType: ${value.EncryptionType}`);
-      console.log(`SequenceNumber: ${value.SequenceNumber}`);
-    })
-    .catch(reason => {
-      console.log(reason);
-    });
 };
 
 const putDynamoDb = item => {
@@ -49,6 +27,33 @@ const putDynamoDb = item => {
     })
     .catch(err => {
       console.log(err);
+    });
+};
+
+const getQueueUrl = context => {
+  const accountId = context.invokedFunctionArn.split(":")[4];
+  const queueName = process.env.SLOT_QUEUE_NAME;
+  var params = {
+    QueueName: queueName /* required */,
+    QueueOwnerAWSAccountId: accountId
+  };
+  return new SQS().getQueueUrl(params).promise();
+};
+
+const putSqsMessage = (queueUrl: string, data: string) => {
+  const sqs = new SQS();
+  const params = {
+    MessageBody: data /* required */,
+    QueueUrl: queueUrl /* required */
+  };
+  return sqs
+    .sendMessage(params)
+    .promise()
+    .then(value => {
+      console.log(value);
+    })
+    .catch(reason => {
+      console.log(reason);
     });
 };
 
@@ -83,7 +88,7 @@ export const addUrl: APIGatewayProxyHandler = async event => {
     });
 };
 
-export const slot: APIGatewayProxyHandler = async () => {
+export const slot: APIGatewayProxyHandler = async (_, context) => {
   const scanParams: DynamoDB.DocumentClient.ScanInput = {
     TableName: process.env.URL_DYNAMODB_TABLE
   };
@@ -98,14 +103,14 @@ export const slot: APIGatewayProxyHandler = async () => {
     });
   console.log(`urls: ${urls}`);
   const revision = new Date().getTime();
+  const queueUrl = (await getQueueUrl(context)).QueueUrl;
   for (const url of urls) {
     const data = {
       url,
       revision
     };
     console.log(`JSON.stringify(data): ${JSON.stringify(data)}`);
-    console.log(`JSON.stringify(data): ${JSON.stringify(data)}`);
-    await putKinesisStream(JSON.stringify(data));
+    await putSqsMessage(queueUrl, JSON.stringify(data));
   }
   return {
     statusCode: 200,
@@ -115,15 +120,10 @@ export const slot: APIGatewayProxyHandler = async () => {
   };
 };
 
-export const crawl: KinesisStreamHandler = async event => {
+export const crawl = async event => {
   for (const record of event.Records) {
-    console.log(`record.kinesis.data: ${record.kinesis.data}`);
-    const data = JSON.parse(
-      new Buffer(record.kinesis.data, "base64").toString()
-    );
-    console.log(`record.kinesis.data(decoded): ${data.url}`);
+    const data = JSON.parse(record.body);
     const html = await getPage(data.url);
-    console.log(`html: ${html}`);
     const crawled_at = new Date().toISOString();
     const item = {
       url: data.url,
