99a6c3ee7960356234cd88e9b141e974c0a7dd39
kytiken <kytiken@gmail.com>
2019-03-03 11:09:01 +0900

    implement dynamodb

--- handler.ts
+++ handler.ts
@@ -1,5 +1,6 @@
 import { APIGatewayProxyHandler, KinesisStreamHandler } from "aws-lambda";
-import { Kinesis } from "aws-sdk";
+import { Kinesis, DynamoDB } from "aws-sdk";
+import { v4 as uuid } from "uuid";
 const rp = require("request-promise");
 
 const getPage = (url: string) => {
@@ -29,6 +30,27 @@ const putKinesisStream = (data: string) => {
     });
 };
 
+const putDynamoDb = item => {
+  const newId = uuid();
+  const dynamoDb = new DynamoDB.DocumentClient();
+  const putParams: DynamoDB.DocumentClient.PutItemInput = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: newId,
+      ...item
+    }
+  };
+  return dynamoDb
+    .put(putParams)
+    .promise()
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
 export const hello: APIGatewayProxyHandler = async event => {
   const urls = [
     "http://v150-95-157-102.a096.g.tyo1.static.cnode.io",
@@ -41,9 +63,11 @@ export const hello: APIGatewayProxyHandler = async event => {
     "http://v150-95-157-102.a096.g.tyo1.static.cnode.io",
     "http://v150-95-157-102.a096.g.tyo1.static.cnode.io"
   ];
+  const revision = new Date().getTime();
   for (const url of urls) {
     const data = {
-      url
+      url,
+      revision
     };
     await putKinesisStream(JSON.stringify(data));
   }
@@ -64,5 +88,13 @@ export const kinesisoutput: KinesisStreamHandler = async event => {
     console.log(`record.kinesis.data(decoded): ${data.url}`);
     const html = await getPage(data.url);
     console.log(`html: ${html}`);
+    const crawled_at = new Date().toISOString();
+    const item = {
+      url: data.url,
+      revision: data.revision,
+      html,
+      crawled_at
+    };
+    await putDynamoDb(item);
   }
 };
