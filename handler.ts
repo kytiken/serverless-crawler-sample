1ef23f06fad3bad5a43021006a8d01a4dbb3cb84
kytiken <kytiken@gmail.com>
2019-03-03 10:17:41 +0900

    implement request

--- handler.ts
+++ handler.ts
@@ -1,16 +1,22 @@
 import { APIGatewayProxyHandler, KinesisStreamHandler } from "aws-lambda";
 import { Kinesis } from "aws-sdk";
+const rp = require("request-promise");
 
-export const hello: APIGatewayProxyHandler = async event => {
-  const kinesis = new Kinesis();
+const getPage = (url: string) => {
+  return rp.get(url).then(htmlString => {
+    return htmlString;
+  });
+};
+
+const putKinesisStream = (data: string) => {
   const kinesisStreamName = process.env.kinesisStreamName;
   const params: Kinesis.PutRecordInput = {
-    Data:
-      "hello" /* required */ /* Strings will be Base-64 encoded on your behalf */,
+    Data: data /* required */ /* Strings will be Base-64 encoded on your behalf */,
     PartitionKey: "hello-partition" /* required */,
     StreamName: kinesisStreamName /* required */
   };
-  await kinesis
+  const kinesis = new Kinesis();
+  return kinesis
     .putRecord(params)
     .promise()
     .then(value => {
@@ -21,24 +27,42 @@ export const hello: APIGatewayProxyHandler = async event => {
     .catch(reason => {
       console.log(reason);
     });
+};
+
+export const hello: APIGatewayProxyHandler = async event => {
+  const urls = [
+    "http://v150-95-157-102.a096.g.tyo1.static.cnode.io",
+    "http://v150-95-157-102.a096.g.tyo1.static.cnode.io",
+    "http://v150-95-157-102.a096.g.tyo1.static.cnode.io",
+    "http://v150-95-157-102.a096.g.tyo1.static.cnode.io",
+    "http://v150-95-157-102.a096.g.tyo1.static.cnode.io",
+    "http://v150-95-157-102.a096.g.tyo1.static.cnode.io",
+    "http://v150-95-157-102.a096.g.tyo1.static.cnode.io",
+    "http://v150-95-157-102.a096.g.tyo1.static.cnode.io",
+    "http://v150-95-157-102.a096.g.tyo1.static.cnode.io"
+  ];
+  for (const url of urls) {
+    const data = {
+      url
+    };
+    await putKinesisStream(JSON.stringify(data));
+  }
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message:
-        "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
       input: event
     })
   };
 };
 
 export const kinesisoutput: KinesisStreamHandler = async event => {
-  event.Records.forEach(record => {
+  for (const record of event.Records) {
     console.log(`record.kinesis.data: ${record.kinesis.data}`);
-    console.log(
-      `record.kinesis.data(decoded): ${new Buffer(
-        record.kinesis.data,
-        "base64"
-      ).toString()}`
+    const data = JSON.parse(
+      new Buffer(record.kinesis.data, "base64").toString()
     );
-  });
+    console.log(`record.kinesis.data(decoded): ${data.url}`);
+    const html = await getPage(data.url);
+    console.log(`html: ${html}`);
+  }
 };
